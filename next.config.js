e592ab53aa5509758a502925d23ef19c5daebbce
Alexander Georgiev <103534450+Alexander6560@users.noreply.github.com>
2026-02-09 19:16:34 +0200

    fix: finalize config for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -16,9 +15,7 @@ const nextConfig = {
       },
     ],
   },
-  // For GitHub Pages deployment
-  basePath: process.env.NODE_ENV === "production" ? "" : "",
-  trailingSlash: true,
+  // ТУК ИЗТРИЙ basePath И trailingSlash
 }
 
 module.exports = nextConfig
